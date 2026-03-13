a342e22af590f9f0842c493e96ebdc3d8d9085f8
lulultfh <punyalulu05@gmail.com>
2026-03-13 15:20:53 +0700

    feat: menambahkan konstanta warna pada main layout

--- lib/main_layout.dart
+++ lib/main_layout.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 
 class MainLayout extends StatelessWidget {
+  static const Color primaryColor = Color(0xff4d51ed);
+  static const Color backgroundColor = Color(0xffffffff);
+  static const Color accentYellow = Color(0xffffc107);
+  static const Color accentOrange = Color(0xffff7043);
+
+  static const Color textTileColor = Color(0xff2d2d2d);
+  static const Color textSubtitleColor = Color(0xff666666);
+  static const Color inputFillColor = Color(0xfff8f9fa);
+  static const Color inputBorderColor = Color(0xffe0e0e0);
+  static const Color labelColor = Color(0xff4a4a4a);
+
   const MainLayout({super.key});
 
   @override
