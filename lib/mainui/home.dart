f01f865c0cf81b94add81b522360e20ab7386b96
lulultfh <punyalulu05@gmail.com>
2026-03-13 18:42:48 +0700

    feat: menambahkan implementasi Main Layout dan tombol logout dengan navigasi pada halaman home

--- lib/mainui/home.dart
+++ lib/mainui/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:navigation_209/main_layout.dart';
 
 class HomePage extends StatefulWidget {
   final Map<String, dynamic>? recentOrder;
@@ -12,6 +13,43 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MainLayout(
+      title: 'Home',
+      showAppBar: true,
+      actions: [
+        IconButton(
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                title: const Text('Logout'),
+                content: const Text('Are you sure want to log out?'),
+                actions: [
+                  TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: const Text('Cancel'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamedAndRemoveUntil(
+                        context,
+                        '/login',
+                        (route) => false,
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.redAccent,
+                      foregroundColor: Colors.white,
+                    ),
+                    child: const Text('Logout'),
+                  ),
+                ],
+              ),
+            );
+          },
+          icon: const Icon(Icons.logout_rounded),
+        ),
+      ],
+    );
   }
 }
