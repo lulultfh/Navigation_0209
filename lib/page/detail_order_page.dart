be163d497aef7f89c5afc14136b41863236a1cc3
lulultfh <punyalulu05@gmail.com>
2026-03-13 19:48:00 +0700

    feat: menambahkan method _buildReseiptRow dengan parameter label, value, dan isTotal pada halaman Detail Order

--- lib/page/detail_order_page.dart
+++ lib/page/detail_order_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:navigation_209/main_layout.dart';
+
 class DetailOrderPage extends StatelessWidget {
   final String makanan;
   final String minuman;
@@ -13,7 +15,38 @@ class DetailOrderPage extends StatelessWidget {
     required this.jumlahMakanan,
     required this.jumlahMinuman,
     required this.totalHarga,
-    });
+  });
+
+  Widget _buildReceiptRow(String label, String value, {bool isTotal = false}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            label,
+            style: TextStyle(
+              fontSize: isTotal ? 18 : 15,
+              fontWeight: isTotal ? FontWeight.bold : FontWeight.w500,
+              color: isTotal
+                  ? MainLayout.primaryColor
+                  : MainLayout.textSubtitleColor,
+            ),
+          ),
+          Text(
+            value,
+            style: TextStyle(
+              fontSize: isTotal ? 20 : 15,
+              fontWeight: isTotal ? FontWeight.w900 : FontWeight.bold,
+              color: isTotal
+                  ? MainLayout.primaryColor
+                  : MainLayout.textTileColor,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
