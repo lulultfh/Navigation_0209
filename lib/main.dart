fe28c6fb9748dd5ef673174ed2024df3ca3a9fd2
lulultfh <punyalulu05@gmail.com>
2026-03-13 21:36:06 +0700

    feat: menambahkan initialroutes dan route pada main program

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:navigation_209/auth/login.dart';
+import 'package:navigation_209/mainui/home.dart';
+import 'package:navigation_209/page/order_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,6 +33,12 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
+      initialRoute: '/login',
+      routes: {
+        '/login':(context) => const LoginPage(),
+        '/home':(context) => const HomePage(),
+        '/order':(context) => const OrderPage(),
+      },
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
