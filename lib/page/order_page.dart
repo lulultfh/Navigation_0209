d811f43b1f2a754d3d1f8eb9ba1703b3897ca7c1
lulultfh <punyalulu05@gmail.com>
2026-03-13 20:34:16 +0700

    feat: menambahkan OrderPage yang merupakan turunan dari Stateful Widget, menambahkan teks controller, dan method calculateTotalPrice untuk menghitung totalHarga, dan menambahkan method dispose untuk mengatur teks controller

---
+++ lib/page/order_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class OrderPage extends StatefulWidget {
+  const OrderPage({super.key});
+
+  @override
+  State<OrderPage> createState() => _OrderPageState();
+}
+
+class _OrderPageState extends State<OrderPage> {
+  final TextEditingController makananController = TextEditingController();
+  final TextEditingController minumanController = TextEditingController();
+  final TextEditingController jumlahMakananController = TextEditingController();
+  final TextEditingController jumlahMinumanController = TextEditingController();
+  int totalHarga = 0;
+  final _formKey = GlobalKey<FormState>();
+
+  void calculateTotalPrice(){
+    int jumlahMakanan = int.parse(jumlahMakananController.text) ?? 0;
+    int jumlahMinuman = int.parse(jumlahMinumanController.text) ?? 0;
+
+    setState(() {
+      totalHarga = (jumlahMakanan * 32000) + (jumlahMinuman * 5000);
+    });
+  }
+
+  @override
+  void dispose(){
+    makananController.dispose();
+    minumanController.dispose();
+    jumlahMakananController.dispose();
+    jumlahMinumanController.dispose();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
